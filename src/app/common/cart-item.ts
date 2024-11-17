40263b4b0b821c0636c277f53752d973858a83f7
phonghh <longtam20015@gmail.com>
2024-11-17 22:12:26 +0700

    Sectuin 17 18 cart status and cart items CRUD

---
+++ src/app/common/cart-item.ts
@@ -0,0 +1,18 @@
+import { Product } from './product';
+
+export class CartItem {
+  id: number;
+  name: string;
+  imageUrl: string;
+  unitPrice: number;
+  quantity: number;
+
+  constructor(product: Product) {
+    this.id = product.id;
+    this.name = product.name;
+    this.unitPrice = product.unitPrice;
+    this.imageUrl = product.imageUrl;
+
+    this.quantity = 1;
+  }
+}
